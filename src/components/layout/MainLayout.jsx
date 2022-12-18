6ffc889734c1e9698edf82a0dc845fca4ec4b436
romanchuchev <romanok20@gmail.com>
2022-12-18 08:18:53 +0300

    feat: create global loading

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import GlobalLoading from "../common/GlobalLoading";
 
 const MainLayout = () => {
   return (
     <>
       {/* loading */}
+      <GlobalLoading />
       {/* loading */}
       <Box display="flex" minHeight="100ch">
         {/* header */}
